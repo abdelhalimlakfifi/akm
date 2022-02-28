38edd5b3560438339ab9277a68e1481d86231c4b
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2022-02-28 01:31:52 +0100

    edit title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>AKM | جمعية الكشف المنير</title>
+        
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="icon" href="{{ asset('images/logo-whitout-background.png') }}" type="image/x-icon">
 
         <!-- Styles -->
         <style>
