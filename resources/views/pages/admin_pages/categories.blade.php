7806cd948ca54142bcd890f1409168b10a13a1f8
abdelhalim lakfifi <abdelhalimlakfifi@gmail.com>
2022-03-02 15:13:27 +0100

    execute script categorie page

--- resources/views/pages/admin_pages/categories.blade.php
+++ resources/views/pages/admin_pages/categories.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.sidebar')
 
 @section('content')
+    <script src="{{ asset('js/index.js') }}"></script>
+    <script>
+        replaceClass();
+    </script>
     <div class="container">
         <h2>Categories</h2>
     </div>
