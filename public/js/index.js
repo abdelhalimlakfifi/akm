e1e0a22a4954f6002546348aecf777fe4022b3ee
abdelhalim lakfifi <abdelhalimlakifi@gmail.com>
2022-03-03 01:29:17 +0100

    javascript function to replace class

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,13 @@
 
 function replaceClass(element)
 {
-    const elem = document.getElementsByClassName('h3');
-    elem.classList.remove('h3');
-
+    var elem = document.getElementsByTagName('h3');
+    
+    elem[0].outerHTML = '<h5>'+ elem[0].innerHTML +'</h5>';
+    
     elem = document.getElementById(element);
-    elem.classList.add('h3');
+    // console.log(element);
+    
+    // console.log(elem.innerText);
+    elem.outerHTML = '<h3>'+ elem.innerText +'</h3>';
 }
