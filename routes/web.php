c3dce030a418dd530f5af0f98d2b006f9699f236
abdelhalim lakfifi <abdelhalimlakifi@gmail.com>
2022-03-03 01:30:35 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\categorieController;
+use App\Http\Controllers\subscriberController;
+use App\Http\Controllers\eventController;
+use App\Http\Controllers\articleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,8 +27,12 @@ Auth::routes(['register' => false]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => 'auth'], function() {
-    
     Route::get('/admin/get/categorie/add', [categorieController::class, 'index'])->name('get.add.categories');
+    Route::get('/admin/get/subscriber/add', [subscriberController::class, 'index_add'])->name('get.subscriber.add');
+    Route::get('/admin/get/subscriber/update', [subscriberController::class, 'index_update'])->name('get.subscriber.update');
+    Route::get('/admin/get/event/create', [eventController::class, 'index'])->name('get.event.create');
+    Route::get('/admin/get/article/create', [articleController::class, 'index'])->name('get.article.create');
+
 
     Route::get('/hello', function () {
         return view('welcome');
