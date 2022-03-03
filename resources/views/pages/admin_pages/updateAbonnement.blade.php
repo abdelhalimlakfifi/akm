84644423cfafc03dd77991ad3b769b43774842b9
abdelhalim lakfifi <abdelhalimlakifi@gmail.com>
2022-03-03 01:30:25 +0100

    update blade

---
+++ resources/views/pages/admin_pages/updateAbonnement.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.sidebar')
+
+@section('content')
+    <script src="{{ asset('js/index.js') }}"></script>
+    <script>
+        replaceClass('updateAbonnement');
+    </script>
+    <div class="container">
+        <h2>updateAbonnement</h2>
+    </div>
+@endsection
