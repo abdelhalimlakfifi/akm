95a7bbfaff4ae132f90442bd6582f3bdac1372d1
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2022-03-19 09:53:13 +0100

    refactor Code

--- app/Http/Controllers/articleController.php
+++ app/Http/Controllers/articleController.php
@@ -17,72 +17,38 @@ class articleController extends Controller
         $domains = Domain::all();
         return view('pages/admin_pages/articles', ['domains' => $domains]);
     }
-    // public function test(Request $request)
-    // {
-    //     //dd($request->all());
-    //     return view('test', ['text' => $request->contentTextArea]);
-    // }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
+    public function create()
+    {
+        //
+    }
+
+
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
