467572809aaa2a6c25022f75a4efcc5ca8a9310e
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2022-03-21 00:13:08 +0100

    refactor

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,14 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/admin/get/subscriber/add', [subscriberController::class, 'index_add'])->name('get.subscriber.add');
     Route::get('/admin/get/subscriber/update', [subscriberController::class, 'index_update'])->name('get.subscriber.update');
     Route::get('/admin/get/event/create', [eventController::class, 'index'])->name('get.event.create');
-    Route::get('/admin/get/article/create', [articleController::class, 'index'])->name('get.article.create');
-
+    
     // Categories
     Route::get('/admin/get/categorie/add', [categorieController::class, 'index'])->name('get.add.categories');
     Route::post('/admin/post/categorie/add', [categorieController::class, 'store'])->name('post.add.categorie');
     Route::post('/admin/post/categorie/update/{id}', [categorieController::class, 'update'])->name('post.update.categorie');
     Route::delete('/admin/post/categorie/delete/{id}', [categorieController::class, 'destroy']);
-    Route::post('/test', [articleController::class, 'test']);
-    Route::get('/hello', function () {
-        return view('welcome');
-    });
+    // Articles
+    Route::get('/admin/get/article/create', [articleController::class, 'index'])->name('get.article.create');
+    Route::post('/admin/post/article/create', [articleController::class, 'store'])->name('post.article.create');
+
 });
