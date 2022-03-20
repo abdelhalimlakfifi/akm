6c889b6e6411a2fecbc927485c14b1e815cf774f
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2022-03-21 00:12:43 +0100

    fix returning 0 on custom id

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    public $incrementing = false;
     protected $fillable = [
         'name',
         'email',
