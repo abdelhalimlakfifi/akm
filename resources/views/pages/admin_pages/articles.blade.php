ccc2184d389119abd493a3556c3ccccada9736c3
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2022-03-21 00:12:58 +0100

    fix select option name

--- resources/views/pages/admin_pages/articles.blade.php
+++ resources/views/pages/admin_pages/articles.blade.php
@@ -9,22 +9,22 @@
 <div class="container d-flex justify-content-center" dir="rtl">
     
 
-    <form method="post" action="/test" class="w-50">
+    <form method="post" action="{{ route('post.article.create') }}" class="w-50" enctype="multipart/form-data">
         @csrf
         <div class="form-group text-center">
             <h2>كتابة مقال</h2>
         </div>
         <div class="form-group">
             <label for="title">عنوان</label>
-            <input type="text" class="form-control" id="title" placeholder="عنوان">
+            <input type="text" name="title" class="form-control" id="title" placeholder="عنوان">
         </div>
         <div class="form-group mt-3 mb-3">
             <label for="files"> إختيار الصورة الرئسية </label>
-            <input type="file" name="files" class="form-control-file" id="files" accept="image/png, image/gif, image/jpeg">
+            <input type="file" name="principle_image" class="form-control-file" id="principle_image" accept="image/png, image/gif, image/jpeg">
         </div>
         <div class="form-group mt-3 mb-3">
             <label for="files"> إختيار صور </label>
-            <input type="file" name="files" class="form-control-file" id="files" multiple accept="image/png, image/gif, image/jpeg, video/mp4,video/x-m4v,video/*">
+            <input type="file" name="files[]" class="form-control-file" id="files" multiple accept="image/png, image/gif, image/jpeg, video/mp4,video/x-m4v,video/*">
         </div>
         <div class="form-group">
             <label for="exampleFormControlTextarea1">  </label>
@@ -32,10 +32,9 @@
         </div>
         <div class="form-group">
             <label for="exampleFormControlSelect2">Example multiple select</label>
-            <select class="form-control" id="exampleFormControlSelect2">
+            <select class="form-control" id="exampleFormControlSelect2" name="domain">
                 @foreach($domains as $domain)
-                <option>{{ $domain->label }}</option>
-                
+                    <option value="{{ $domain->id }}">{{ $domain->label }}</option>
                 @endforeach
             </select>
         </div>
