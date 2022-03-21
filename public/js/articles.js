7f909fdbc5edf41968de3a6aa8ab66bde5ebfbfc
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2022-03-21 10:48:26 +0100

    create js file for articles

---
+++ public/js/articles.js
@@ -0,0 +1,20 @@
+
+    // $('#article_form').submit(function(event){
+    //     console.log("111");
+    //     event.preventDefault();
+    //     $.ajax({
+    //         url:'/admin/post/article/create',
+    //         type:'POST',
+    //         data:new FormData(this),
+    //         dataType:'JSON',
+    //         contentType: false,
+    //         cache: false,
+    //         processData: false,
+    //         success:function(data)
+    //                 {
+    //                     alert("Done")
+    //                 }
+            
+    //     })
+    // })
+
