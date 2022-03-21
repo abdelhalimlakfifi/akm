267eb1dd33c9731aee8aabfa55b9011bac19dbb0
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2022-03-21 23:42:16 +0100

    fix domain atribut

--- app/Http/Controllers/articleController.php
+++ app/Http/Controllers/articleController.php
@@ -18,15 +18,14 @@ class articleController extends Controller
 
     public function store(Request $request)
     {
-        
+        //dd($request->contentTextArea);
+        //dd(intval($request->domainOption));
         $now = date('_Y_m_d_H_i_s');
         $title = $request->title;
         //dd($title);
         $filesPath = str_replace(' ','_',$title).$now;
         //dd($request->file('principleImage'));
-        $request->file('principleImage')
-                ->storeAs('articles/'.$filesPath.'/principle',$request->file('principleImage')->getClientOriginalName());
-
+        $request->file('principleImage')->storeAs('articles/'.$filesPath.'/principle',$request->file('principleImage')->getClientOriginalName());
         if(intval($request->totalFiles) > 0)
         {
             for($i = 0; $i < intval($request->totalFiles); $i++)
@@ -43,7 +42,7 @@ class articleController extends Controller
         $article->file_path = $filesPath;
         $article->content   = $request->contentTextArea;
         $article->user_id   = Auth::id();
-        $article->domain_id = intval($request->domain);
+        $article->domain_id = intval($request->domainOption);
         $article->save();
 
         return response()->json($article);
