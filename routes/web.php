b3c20166d325c24003f49af84c355f60da68ae00
abdelhalim lakfifi <abdelhalimlakfifi@gmail.com>
2022-03-24 18:21:03 +0100

    add routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Auth::routes(['register' => false]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => 'auth'], function() {
-    Route::get('/admin/get/subscriber/add', [subscriberController::class, 'index_add'])->name('get.subscriber.add');
     Route::get('/admin/get/subscriber/update', [subscriberController::class, 'index_update'])->name('get.subscriber.update');
     Route::get('/admin/get/event/create', [eventController::class, 'index'])->name('get.event.create');
     
@@ -30,5 +29,11 @@ Route::group(['middleware' => 'auth'], function() {
     // Articles
     Route::get('/admin/get/article/create', [articleController::class, 'index'])->name('get.article.create');
     Route::post('/admin/post/article/create', [articleController::class, 'store'])->name('post.article.create');
+
+
+    //users
+    Route::get('/admin/get/subscribers', [subscriberController::class, 'index'])->name('get.subscribers');
+    Route::get('/admin/get/subscriber/add', [subscriberController::class, 'index_add'])->name('get.subscriber.add');
+
 
 });
