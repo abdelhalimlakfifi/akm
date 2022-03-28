fcf24ab374de46e488bf88b303aea6d91a9bc5e8
abdelhalim lakfifi <abdelhalimlakfifi@gmail.com>
2022-03-28 17:17:40 +0000

    refactor code

--- app/Http/Controllers/subscriberController.php
+++ app/Http/Controllers/subscriberController.php
@@ -6,15 +6,13 @@ use Illuminate\Http\Request;
 use App\Models\Domain;
 class subscriberController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         return view('pages/admin_pages/abonnements');
     }
+
+    
     public function index_add()
     {   
         $domains = Domain::all();
@@ -22,72 +20,44 @@ class subscriberController extends Controller
         // dd($domains);
         return view('pages/admin_pages/addAbonnement', ["domains" => $domains, "index" => 0]);
     }
+
+
     public function index_update()
     {
         return view('pages/admin_pages/updateAbonnement');
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
         //
