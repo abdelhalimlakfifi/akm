f75ee5cf09af8b0e337c69d3887411c295330436
abdelhalim lakfifi <abdelhalimlakfifi@gmail.com>
2022-03-29 17:18:16 +0000

    add _token

--- public/js/add_abonnement.js
+++ public/js/add_abonnement.js
@@ -10,10 +10,37 @@ function formSubmit(event)
     let birthday    = $('#birthday').val();
     let price       = $('#price').val();
     let numOfMonths = $('#numOfMonths').val();
+    let _token = $("input[name=_token]").val();
 
     let checkedValues = []
     $('input:checkbox[name=checkBoxes]:checked').each(function(){
         checkedValues.push($(this).val());
     });
+    if(checkedValues.length > 0)
+    {
+        $.ajax({
+        url: "/admin/post/subscriber/add",
+            type: "POST",
+            data: {
+                fullname,
+                email,
+                phone,
+                cin,
+                sex,
+                birthday,
+                price,
+                numOfMonths,
+                checkedValues,
+                _token
+            },
+            success: function(res){
     
+            },
+            error: function(res){
+    
+            }
+        })
+    } else {
+        alert("المرجو اختيار على الاقل شعبة واحدة");
+    }
 }
