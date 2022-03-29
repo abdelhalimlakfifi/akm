b5168afc7b2b35280edc27fd4b77d777626a5d71
abdelhalim lakfifi <abdelhalimlakfifi@gmail.com>
2022-03-29 12:10:48 +0000

    refactr code

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/admin/get/subscriber/update', [subscriberController::class, 'index_update'])->name('get.subscriber.update');
     Route::get('/admin/get/event/create', [eventController::class, 'index'])->name('get.event.create');
-    
     // Categories
     Route::get('/admin/get/categorie/add', [categorieController::class, 'index'])->name('get.add.categories');
     Route::post('/admin/post/categorie/add', [categorieController::class, 'store'])->name('post.add.categorie');
@@ -29,11 +28,7 @@ Route::group(['middleware' => 'auth'], function() {
     // Articles
     Route::get('/admin/get/article/create', [articleController::class, 'index'])->name('get.article.create');
     Route::post('/admin/post/article/create', [articleController::class, 'store'])->name('post.article.create');
-
-
     //users
     Route::get('/admin/get/subscribers', [subscriberController::class, 'index'])->name('get.subscribers');
     Route::get('/admin/get/subscriber/add', [subscriberController::class, 'index_add'])->name('get.subscriber.add');
-
-
 });
